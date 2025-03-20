9d217687387f19a5a743f18bc990179ce118e0a5
Barbara  Samira Santiesteban Saravia <145080442+BarbaraSamiraS@users.noreply.github.com>
2025-03-20 18:58:33 +0000

    Alan

--- App.js
+++ App.js
@@ -1,84 +1,98 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <View style={styles.infos}>
-        <Text style={styles.tituloText}>Alan Turing</Text>
-        <Image 
-          style ={styles.imagem}
-          source = {require('./assets/alanTuring.jpg')}>
-        </Image>
+    <View style={styles.box}>
+      <View style={styles.topo}>
+        <View style={styles.icon}>
+          <Ionicons name="arrow-back" size={25} color="#696969"/>
+        </View>
+        <View style={styles.barraPesquisa}>
+          <Ionicons name="search" size={17} color="#696969" />
+          <Text>Alan Turing</Text>
+        </View>
       </View>
-
-      <ScrollView style={styles.area}>
-        <Text style={styles.texts}>
-        Alan Mathison Turing nasceu no dia 23 de junho de 1912, em um bairro residencial de Londres, capital da Inglaterra. Seu pai, Julius Mathison Turing, era um oficial que trabalhava na Madras Presidency.  região administrativa criada pelos britânicos na Índia britânica.
-
- Alan Turing, considerado atualmente como o pai da computação, foi um matemático e criptógrafo inglês considerado, por meio de suas ideias, foi possível desenvolver o que chamamos hoje de computador. Turing também ficou muito conhecido como um dos responsáveis por decifrar o código utilizado pelas comunicações nazistas durante a Segunda Guerra Mundial. Por meio do seu trabalho, foi desenvolvida uma máquina conhecida como “bomba eletromecânica” (The Bombe, em inglês), que decifrou o código da máquina Enigma utilizado pelos alemães, e permitiu que os Aliados tivessem acesso a informações privilegiadas ao longo da guerra.
-
-Durante sua infância, Turing estudou em diversas escolas, tais como Hazelhurst Preparatory School e Sherborne School. Na Sherborne ingressou quando tinha 13 anos, e um episódio peculiar marcou sua entrada nela. No seu primeiro dia de aula, aconteceu uma greve geral na Grã-Bretanha que o impediu ir de trem. Turing resolveu, em sua bicicleta, percorrer os 100 km que separavam a escola de sua casa. Alguns estudos feitos sobre a vida de Turing mostram que, em Sherborne, ele logo demonstrou grande interesse pela matemática, e, apesar de ser reconhecido atualmente como um gênio, algumas de suas notas eram apenas regulares. Em Sherborne, conheceu Christopher Morcom, o qual muitos acreditam ter sido seu primeiro amor. Morcom, porém, faleceu em 1930, em decorrência de tuberculose bovina. A essa altura, Turing tinha 18 anos de idade.
-
-
-O maior legado deixado pelo matemático Alan Turing é, sem dúvidas, a invenção da máquina de Turing. Esta é um modelo teórico que pode ser usado para implementar todos os aspectos lógicos e matemáticos de um computador, independentemente de como ele venha a ser construído (mecânica ou eletronicamente, por exemplo).
-
-Além de ter construído as bases da computação moderna, Turing também desenvolveu os primeiros testes capazes de distinguir a inteligência artificial da inteligência humana. Atualmente os testes de Turing são usados em diversos sites e dispositivos, promovendo maior segurança para os seus usuários.
-Turing morreu em 1954, provavelmente tendo cometido suicídio. 
-
-        </Text>
-      </ScrollView>
-
-      <StatusBar style="auto" />
+      <View style={styles.painel}></View>
+        <Image 
+            style ={styles.imagem}
+            source = {require('./assets/alanTuring.jpg')}>
+        </Image>
+        <Image 
+            style ={styles.circulo}
+            source = {require('./assets/alanTuring.jpg')}>
+        </Image>
+      <View style={styles.container}>
+        <View style={styles.titulo}>
+          <Text style={styles.tituloNome}>Alan Turing</Text>
+        </View>
+        <View style={styles.desc}>
+          <Text>Matemático e Cientista da Computação | Pioneiro da Inteligência Artificial e da Criptografia</Text>
+        </View>
+        <View style={styles.universidade}>
+          <Text>Government Code and Cypher School (GC&CS) - Bletchley Park</Text>
+          <Text>Inglaterra</Text>
+        </View>
+        <View style={styles.conexao}>
+          <Text>+ de 500 </Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  box: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    paddingTop: 30,
+    paddingTop: 25,
   },
-
-  infos: {
+  topo:{
+    width: '100%',
     display: 'flex',
     flexDirection: 'row',
-    width: '90%',
-    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 50,
   },
-
-  tituloText : {
-    color: '#6b3601',
-    fontSize: 50,
-    fontWeight: 'bold',
-    fontFamily: '',
-    maxHeight:30,
+  icon:{
+    width: '15%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-
+  barraPesquisa:{
+    width: '82%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 34,
+    backgroundColor: '#d3dff0',
+    paddingLeft: 5,
+    gap: 5,
+  },
+  container:{
+    width:'100%',
+    padding:10,
+    gap: 15,
+  },
   imagem:{
-    width:100,
-    height:150,
-    marginBottom: 20,
-    borderRadius: 10,
-    },
-
-  area:{
-    backgroundColor: '#6b3601',
-    color: '#ff9',
-    borderRadius: 10,
-    fontFamily: 'arial',
+    width:'100%',
+    height:'18%',
+  },
+  circulo:{
+    width: 90,
+    height: 90,
+    marginLeft: 15,
+    borderRadius: '100%',
+    borderColor: '#FFF',
+    borderWidth: 4,
+    borderStyle: 'solid',
+    top: -39,
+  },
+  tituloNome:{
+    fontSize: 20,
     fontWeight: 'bold',
-    padding: 15,
-    textAlign: 'justify',
-    width: '90%',
-    maxHeight: '64%',
-    },
-
-    texts:{
-      color: '#fff',
-    },
-
+  },
 });
